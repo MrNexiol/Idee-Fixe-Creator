4314057e69803d79cd7bf8758b966effc73f8f0c
Tomasz Kopyciński <tkopycinski@prograils.com>
2021-11-16 13:14:14 +0100

    Added method to get database instance

--- app/src/main/java/kopycinski/tomasz/ideefixecreator/database/AppDatabase.kt
+++ app/src/main/java/kopycinski/tomasz/ideefixecreator/database/AppDatabase.kt
@@ -1,6 +1,8 @@
 package kopycinski.tomasz.ideefixecreator.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import kopycinski.tomasz.ideefixecreator.database.dao.*
 import kopycinski.tomasz.ideefixecreator.database.entity.*
@@ -22,4 +24,21 @@ abstract class AppDatabase : RoomDatabase() {
     abstract fun specializationDao(): SpecializationDao
     abstract fun advantageDao(): AdvantageDao
     abstract fun equipmentDao(): EquipmentDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        AppDatabase::class.java,
+                        "app_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
 }

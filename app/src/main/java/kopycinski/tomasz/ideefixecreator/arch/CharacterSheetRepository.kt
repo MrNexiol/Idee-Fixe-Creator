e73d1177de2ab41e59c675dc8e15045112259da0
Tomasz Kopyciński <tkopycinski@prograils.com>
2021-11-16 13:23:13 +0100

    Added hilt providers for database and character sheet dao Added basic character sheet repository

---
+++ app/src/main/java/kopycinski/tomasz/ideefixecreator/arch/CharacterSheetRepository.kt
@@ -0,0 +1,19 @@
+package kopycinski.tomasz.ideefixecreator.arch
+
+import kopycinski.tomasz.ideefixecreator.database.dao.CharacterSheetDao
+import kopycinski.tomasz.ideefixecreator.database.entity.CharacterSheet
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class CharacterSheetRepository @Inject constructor(
+    private val characterSheetDao: CharacterSheetDao
+) {
+    suspend fun insertCharacterSheet(characterSheet: CharacterSheet) =
+        characterSheetDao.insertAll(characterSheet)
+
+    suspend fun updateCharacterSheet(characterSheet: CharacterSheet) =
+        characterSheetDao.updateAll(characterSheet)
+
+    fun getCharacterSheet(id: Long): Flow<CharacterSheet> =
+        characterSheetDao.getOne(id)
+}
--- app/src/main/java/kopycinski/tomasz/ideefixecreator/di/HiltModule.kt
+++ app/src/main/java/kopycinski/tomasz/ideefixecreator/di/HiltModule.kt
@@ -1,11 +1,24 @@
 package kopycinski.tomasz.ideefixecreator.di
 
+import android.content.Context
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import kopycinski.tomasz.ideefixecreator.database.AppDatabase
+import kopycinski.tomasz.ideefixecreator.database.dao.CharacterSheetDao
 
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class HiltModule {
+    @Provides
+    fun provideCharacterSheetDao(appDatabase: AppDatabase): CharacterSheetDao {
+        return appDatabase.characterSheetDao()
+    }
 
+    @Provides
+    fun provideAppDatabase(@ApplicationContext context: Context): AppDatabase {
+        return AppDatabase.getDatabase(context)
+    }
 }

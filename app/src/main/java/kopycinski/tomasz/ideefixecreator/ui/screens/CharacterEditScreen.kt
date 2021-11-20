0864c3df948dbd933b7bc9c643812c1eb2e40789
Tomasz Kopyciński <tkopycinski@prograils.com>
2021-11-20 19:38:10 +0100

    Added dummy text to edit character sheet screen

--- app/src/main/java/kopycinski/tomasz/ideefixecreator/ui/screens/CharacterEditScreen.kt
+++ app/src/main/java/kopycinski/tomasz/ideefixecreator/ui/screens/CharacterEditScreen.kt
@@ -1,9 +1,13 @@
 package kopycinski.tomasz.ideefixecreator.ui.screens
 
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
+import kopycinski.tomasz.ideefixecreator.ui.theme.IdeeFixeCreatorTheme
 
 @Composable
 fun CharacterEditScreen(navController: NavController) {
-
+    IdeeFixeCreatorTheme {
+        Text(text = "That all folks")
+    }
 }

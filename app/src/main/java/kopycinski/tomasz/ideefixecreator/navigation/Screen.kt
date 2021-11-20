00ce3a21c909908b7519150610c9a1710e4938a7
Tomasz Kopyciński <tkopycinski@prograils.com>
2021-11-20 14:01:43 +0100

    Added screen class Added screen name string resources

---
+++ app/src/main/java/kopycinski/tomasz/ideefixecreator/navigation/Screen.kt
@@ -0,0 +1,11 @@
+package kopycinski.tomasz.ideefixecreator.navigation
+
+import androidx.annotation.StringRes
+import kopycinski.tomasz.ideefixecreator.R
+
+sealed class Screen(val route: String, @StringRes val resourceId: Int) {
+    object MainScreen : Screen("main_screen", R.string.main_screen)
+    object CharacterListScreen : Screen("character_list_screen", R.string.character_list_screen)
+    object CharacterCreateScreen : Screen("character_create_screen", R.string.character_create_screen)
+    object CharacterEditScreen : Screen("character_edit_screen", R.string.character_edit_screen)
+}

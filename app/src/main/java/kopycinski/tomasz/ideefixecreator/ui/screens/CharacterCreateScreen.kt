d90a0737d478d31ff65924d2150dfebaf987866b
Tomasz Kopyciński <tkopycinski@prograils.com>
2021-11-21 16:29:26 +0100

    Added dependency for navigation with hilt Added saving dummy character sheet on button click in character create screen

--- app/src/main/java/kopycinski/tomasz/ideefixecreator/ui/screens/CharacterCreateScreen.kt
+++ app/src/main/java/kopycinski/tomasz/ideefixecreator/ui/screens/CharacterCreateScreen.kt
@@ -1,17 +1,29 @@
 package kopycinski.tomasz.ideefixecreator.ui.screens
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import kopycinski.tomasz.ideefixecreator.database.entity.CharacterSheet
 import kopycinski.tomasz.ideefixecreator.navigation.Screen
 import kopycinski.tomasz.ideefixecreator.ui.theme.IdeeFixeCreatorTheme
+import kopycinski.tomasz.ideefixecreator.viewmodel.CharacterCreateViewModel
 
 @Composable
-fun CharacterCreateScreen(navController: NavController) {
+fun CharacterCreateScreen(
+    navController: NavController,
+    viewModel: CharacterCreateViewModel = hiltViewModel()
+) {
     IdeeFixeCreatorTheme {
-        Button(onClick = { navController.navigate(Screen.CharacterListScreen.route) }) {
-            Text(text = "Save")
+        Column {
+            Button(onClick = { navController.navigate(Screen.CharacterListScreen.route) }) {
+                Text(text = "Navigate")
+            }
+            Button(onClick = { viewModel.insertCharacterSheet(CharacterSheet(name = "test")) }) {
+                Text(text = "Save")
+            }
         }
     }
 }
--- app/src/main/java/kopycinski/tomasz/ideefixecreator/viewmodel/CharacterCreateViewModel.kt
+++ app/src/main/java/kopycinski/tomasz/ideefixecreator/viewmodel/CharacterCreateViewModel.kt
@@ -1,11 +1,20 @@
 package kopycinski.tomasz.ideefixecreator.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kopycinski.tomasz.ideefixecreator.database.entity.CharacterSheet
 import kopycinski.tomasz.ideefixecreator.database.repository.CharacterSheetRepository
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class CharacterCreateViewModel @Inject constructor(
     private val characterSheetRepository: CharacterSheetRepository
-) : ViewModel()
+) : ViewModel() {
+    fun insertCharacterSheet(characterSheet: CharacterSheet) {
+        viewModelScope.launch {
+            characterSheetRepository.insertCharacterSheet(characterSheet)
+        }
+    }
+}

29e5d7bd0fc1def443eab75f5ef43e9ff3958072
Tomasz Kopyciński <tkopycinski@prograils.com>
2021-11-21 16:45:21 +0100

    Refactored basic character list

--- app/src/main/java/kopycinski/tomasz/ideefixecreator/ui/screens/CharacterListScreen.kt
+++ app/src/main/java/kopycinski/tomasz/ideefixecreator/ui/screens/CharacterListScreen.kt
@@ -1,12 +1,14 @@
 package kopycinski.tomasz.ideefixecreator.ui.screens
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import kopycinski.tomasz.ideefixecreator.database.entity.CharacterSheet
@@ -22,13 +24,34 @@ fun CharacterListScreen(
     val characterList: List<CharacterSheet> by viewModel.getAll().collectAsState(listOf())
 
     IdeeFixeCreatorTheme {
-        LazyColumn() {
-            items(characterList) { characterSheet ->
-                Text(text = characterSheet.name)
-            }
+        CharacterSheetList(
+            characterList = characterList,
+            navController = navController
+        )
+    }
+}
+
+@Composable
+fun CharacterSheetList(
+    characterList: List<CharacterSheet>,
+    navController: NavController
+) {
+    LazyColumn {
+        items(characterList) { characterSheet ->
+            CharacterListItem(
+                characterSheet = characterSheet,
+                navController = navController
+            )
         }
-        Button(onClick = { navController.navigate(Screen.CharacterShowScreen.route) }) {
-            Text(text = "Dummy character sheet")
-        }
+    }
+}
+
+@Composable
+fun CharacterListItem(
+    characterSheet: CharacterSheet,
+    navController: NavController
+) {
+    Row(Modifier.clickable { navController.navigate(Screen.CharacterShowScreen.route) }) {
+        Text(text = characterSheet.name)
     }
 }

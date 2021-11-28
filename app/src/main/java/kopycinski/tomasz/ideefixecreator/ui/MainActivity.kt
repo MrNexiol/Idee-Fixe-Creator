b3d05609696c5e1e4339844503efabd706e480aa
Tomasz Kopyciński <tkopycinski@prograils.com>
2021-11-28 18:48:11 +0100

    Removed unused imports from mainActivity

--- app/src/main/java/kopycinski/tomasz/ideefixecreator/ui/MainActivity.kt
+++ app/src/main/java/kopycinski/tomasz/ideefixecreator/ui/MainActivity.kt
@@ -3,15 +3,8 @@ package kopycinski.tomasz.ideefixecreator.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 import kopycinski.tomasz.ideefixecreator.navigation.Navigation
-import kopycinski.tomasz.ideefixecreator.ui.theme.IdeeFixeCreatorTheme
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {

09dc902b3f3c16e72b27811c620a7cb425327a6f
Tomasz Kopyciński <tkopycinski@prograils.com>
2021-11-29 21:58:11 +0100

    Added showing attribute list in createCharacter screen

--- app/src/main/java/kopycinski/tomasz/ideefixecreator/ui/screens/charactercreatescreen/AttributeTabContent.kt
+++ app/src/main/java/kopycinski/tomasz/ideefixecreator/ui/screens/charactercreatescreen/AttributeTabContent.kt
@@ -1,7 +1,10 @@
 package kopycinski.tomasz.ideefixecreator.ui.screens.charactercreatescreen
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import kopycinski.tomasz.ideefixecreator.viewmodel.CharacterCreateViewModel
 
@@ -10,5 +13,10 @@ fun AttributeTabContent(
     modifier: Modifier = Modifier,
     viewModel: CharacterCreateViewModel
 ) {
-    Text(text = "Attribute")
+    val attributes by viewModel.attributes.collectAsState()
+    Column(modifier = modifier) {
+        attributes.forEach { 
+            Text(text = it.name)
+        }
+    }
 }

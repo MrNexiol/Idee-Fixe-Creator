55d71331a9b343c1330125bba67ad28c4ced6208
Tomasz Kopyciński <tkopycinski@prograils.com>
2021-11-29 21:39:08 +0100

    Moved characterCreateScreen to its own package Added tabContent methods to own files

--- app/src/main/java/kopycinski/tomasz/ideefixecreator/navigation/Navigation.kt
+++ app/src/main/java/kopycinski/tomasz/ideefixecreator/navigation/Navigation.kt
@@ -7,6 +7,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import kopycinski.tomasz.ideefixecreator.ui.screens.*
+import kopycinski.tomasz.ideefixecreator.ui.screens.charactercreatescreen.CharacterCreateScreen
 
 @Composable
 fun Navigation() {
---
+++ app/src/main/java/kopycinski/tomasz/ideefixecreator/ui/screens/charactercreatescreen/AttributeTabContent.kt
@@ -0,0 +1,14 @@
+package kopycinski.tomasz.ideefixecreator.ui.screens.charactercreatescreen
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import kopycinski.tomasz.ideefixecreator.viewmodel.CharacterCreateViewModel
+
+@Composable
+fun AttributeTabContent(
+    modifier: Modifier = Modifier,
+    viewModel: CharacterCreateViewModel
+) {
+    Text(text = "Attribute")
+}

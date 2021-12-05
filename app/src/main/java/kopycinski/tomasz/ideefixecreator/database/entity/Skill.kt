b93ea56e5b8342e930d5443455e00e80c86a5cc6
Tomasz Kopyciński <tkopycinski@prograils.com>
2021-12-05 17:15:17 +0100

    Added baseLevel to skill entity

--- app/src/main/java/kopycinski/tomasz/ideefixecreator/database/entity/Skill.kt
+++ app/src/main/java/kopycinski/tomasz/ideefixecreator/database/entity/Skill.kt
@@ -10,7 +10,8 @@ data class Skill(
     val attributeId: Long,
     val name: String,
     val description: String,
-    var level: Int = 8,
+    var baseLevel: Int = 8,
+    var level: Int = baseLevel,
     var upgradeCost: Int = 1,
 
     @PrimaryKey(autoGenerate = true) val skillId: Long = 0

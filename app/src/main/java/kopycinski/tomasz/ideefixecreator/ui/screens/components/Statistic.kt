9639d05460ea10bd1430c44e818788e5ec5713b6
tomaszowy <mrnexiol@gmail.com>
2023-03-19 07:24:22 +0100

    Added statistic component

---
+++ app/src/main/java/kopycinski/tomasz/ideefixecreator/ui/screens/components/Statistic.kt
@@ -0,0 +1,51 @@
+package kopycinski.tomasz.ideefixecreator.ui.screens.components
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Card
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import kopycinski.tomasz.ideefixecreator.database.entity.recent.Statistic
+
+@Composable
+fun Statistic(
+    statistic: Statistic
+) {
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+    Card(
+        modifier = Modifier
+            .padding(bottom = 4.dp)
+            .border(2.dp, MaterialTheme.colors.primary, MaterialTheme.shapes.large),
+        contentColor = if (expanded) MaterialTheme.colors.background else MaterialTheme.colors.primary,
+        backgroundColor = if (expanded) MaterialTheme.colors.primary else MaterialTheme.colors.background
+    ) {
+        Column {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .clickable { expanded = !expanded }
+                    .padding(8.dp)
+            ) {
+                Text(modifier = Modifier.weight(1F), text = statistic.name)
+            }
+            if (expanded) {
+                Divider(color = MaterialTheme.colors.onPrimary)
+                Text(modifier = Modifier.padding(8.dp), text = statistic.description)
+            }
+        }
+    }
+}
--- app/src/main/java/kopycinski/tomasz/ideefixecreator/ui/screens/debug/AttributesScreen.kt
+++ app/src/main/java/kopycinski/tomasz/ideefixecreator/ui/screens/debug/AttributesScreen.kt
@@ -2,11 +2,10 @@ package kopycinski.tomasz.ideefixecreator.ui.screens.debug
 
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.graphics.Color
 import androidx.hilt.navigation.compose.hiltViewModel
+import kopycinski.tomasz.ideefixecreator.ui.screens.components.Statistic
 import kopycinski.tomasz.ideefixecreator.viewmodel.DebugViewModel
 
 @Composable
@@ -17,8 +16,7 @@ fun AttributesScreen(
 
     LazyColumn {
         items(list) {
-            Text(text = it.name, color = Color.White)
-            Text(text = it.description, color = Color.White)
+            Statistic(statistic = it)
         }
     }
 }
